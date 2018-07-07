73f4d017d32fdf6749660a75326d170ec8bf75f0
Adrian <adrianh.macairan@gmail.com>
2018-07-07 12:59:59 -0400

    cookie constructor

--- lab-04 cookies/main.js
+++ lab-04 cookies/main.js
@@ -1,48 +1,37 @@
-let storeOne = {
-    name: 'Chocolate Store',
-    product: 'Chocolate Cookies',
-    costumers: 150,
-    employees:  5,
-    location: 'Bethesda',
-    createCostumer: function() {
-        console.log(this.product);
-    }
-
+//Constructor
+function Store (name, location, product, costumers){
+    this.storeName = name;
+    this.storeLocation = location;
+    this.storeProduct = product;
+    this.costCount = costumers;
 }
 
-let storeTwo = {
-    name: 'Vanilla Store',
-    product: 'Vanilla Cookies',
-    costumers: 200,
-    employees: 10,
-    location: 'Baltimore',
-    getProduct: function() {
-        console.log(this.product);
-    }
-}
+var storeOne = new Store('Vanilla Store', 'Baltimore', 'Vanilla Cookies', 6);
+var storeTwo = new Store('Chocolate Store','Bethesda', 'Chocolate Cookies', 10);
 
-//array of objects
-let objArray = [storeOne, storeTwo];
 
-//creating elements
-let elBody = document.getElementById('myBody');
-let elTable = document.createElement('table');
-console.log(elTable);
-elTable.setAttribute('class', 'class-table');
-elBody.appendChild(elTable);
+// let storeOne = {
+//     name: 'Chocolate Store',
+//     product: 'Chocolate Cookies',
+//     costumers: 150,
+//     employees:  5,
+//     location: 'Bethesda',
+//     createCostumer: function() {
+//         console.log(this.product);
+//     }
 
-//looping through the array of objects
-for(let i = 0; i < objArray.length; i++) {
-    let elRow = document.createElement('tr');
-    elRow.textContent = objectArray[i].name;
-    
-    
-    
-    
-    console.log(objArray[i].name);
-    console.log(objArray[i].location);
-    console.log(objArray[i].employees);
-    objArray[i].price = 10;
-    objArray[i].revenue = objArray[i].price * objArray[i].costumers;
-    console.log(objArray[i]);
-}
+// }
+
+// let storeTwo = {
+//     name: 'Vanilla Store',
+//     product: 'Vanilla Cookies',
+//     costumers: 200,
+//     employees: 10,
+//     location: 'Baltimore',
+//     getProduct: function() {
+//         console.log(this.product);
+//     }
+// }
+
+// //array of objects
+// let Store = [storeOne, storeTwo];
